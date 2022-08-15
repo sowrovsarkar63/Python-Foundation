662c6f7cb5f74cc712c44f8dc699e3f1e22b0f67
Md sowrov sarkar <dev.sowrovsarkar63@gmail.com>
2022-08-15 15:29:02 +0600

    Complexity

---
+++ complexity.py
@@ -0,0 +1,11 @@
+from cmath import log
+
+print(" ============= PracticeTime And Space Complexity ============")
+
+firstNumber = 100
+secondNumber = 50
+
+sumOfNumber = firstNumber + secondNumber
+
+
+print("Some of number is ", sumOfNumber)  # Complexity of this program is O(1)
